caa2894029a80bc36ce630f55caa712b4a11e0ed
dkok97 <dinkarkhattar@gmail.com>
2017-11-12 22:07:15 -0800

    edited views.py for email_requests

--- src/email_requests/views.py
+++ src/email_requests/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from .models import Request
 from .serializers import RequestSerializer
+from users.models import Profile, Mentor, User
+
 
 # Create your views here.
 class EmailRequestView(generics.RetrieveUpdateDestroyAPIView):
@@ -10,17 +12,30 @@ class EmailRequestView(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self):
         return get_object_or_404(Profile, user=self.request.user)
 
-    def post(self, request, mentor_id):
+    def post(self, *args, **kwargs):
 
-        subject = ""
-        message_body = ""
-        from_email = ""
-        to_email = ""
+        mentor_id = int(self.kwargs['mentor_id'])
+        mentor = get_object_or_404(User, id=mentor_id)
+        mentor_email = mentor.email
+
+        mentee_id = self.request.user.user.id
+        mentee = get_object_or_404(User, id=mentee_id)
+        mentee_name = mentee.first_name + ' ' + mentee.last_name
+        mentee_email = mentee.email
+
+
+        subject = "New Request from PickMyBruin"
+        message_body = "You have a new request from " + mentee_name + "\n" + "Their email is: " + mentee.email "\n"
+        from_email = "pickMyBruin@devx.com"
+        to_email = mentor_email
 
         send_mail(subject, message_body, from_email, [to_email], fail_silently=False)
 
+        '''add request to database (similiar to adding a new user(look at views.py for users))
+            return Response(RequestSerializer(new_request).data)
+        '''
 
-#*****This is all mostly copy paste from view in users^^^
+        return "request saved to database"
 
 
 '''What this needs to do is:
