15fc3b4793831b5689a58dcb8627d0caeab10d2e
DavidWChen <david.willisis.chen@gmail.com>
2017-11-28 20:46:49 -0800

    The Great URL Redesign

--- src/users/urls.py
+++ src/users/urls.py
@@ -12,8 +12,8 @@ router.register(r'majors', views.MajorViewSet)
 router.register(r'mentors', views.MentorViewSet)
 
 urlpatterns = [
-    url(r'me/?$', views.OwnProfileView.as_view(), name='me'),
-    url(r'mentors/?$', views.MentorsSearchView.as_view(), name='mentors_search'),
-    url(r'users/?$', views.CreateUser.as_view(), name='create'),
-    url(r'verify/?$', views.VerifyUser.as_view(), name='verify'),
+    url(r'^users/me/?$', views.OwnProfileView.as_view(), name='me'),
+    url(r'^mentors/?$', views.MentorsSearchView.as_view(), name='mentors_search'),
+    url(r'^users/?$', views.CreateUser.as_view(), name='create'),
+    url(r'^verify/?$', views.VerifyUser.as_view(), name='verify'),
 ]
