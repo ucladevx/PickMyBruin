65da6b305a11f6ee2bd6081a34d46f1a25dcf07a
DavidWChen <DavidWChen@users.noreply.github.com>
2018-01-25 00:40:54 -0800

    Check Sendgrid response (#47)
    * Check Sendgrid Response
    
    * Formatting
    
    * Change conditions

--- src/email_requests/views.py
+++ src/email_requests/views.py
@@ -49,6 +49,8 @@ class EmailRequestView(generics.CreateAPIView):
         mail.template_id = REQUEST_TEMPLATE
         sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_API_KEY)
         response = sg.client.mail.send.post(request_body=mail.get())
+        if not (200 <= response.status_code < 300):
+            raise ValidationError({'status_code': response.status_code})
 
         new_request = Request(
             mentee=mentee_profile,
